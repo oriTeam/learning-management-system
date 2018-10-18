6a80169a5d639fc8108d0a7131b576b56abe4c16
tuanhah <tuantsukuyomi@gmail.com>
2018-10-18 17:11:55 +0700

    change something

---
+++ static/javascripts/userpage/user-page.js
@@ -0,0 +1,10 @@
+
+Vue.component('ex2',{
+    data: function () {
+        return {
+          count: 0
+        }
+      },
+    template : '<label for="example-text-input" class="col-2 col-form-label">Instagram</label>'
+});
+var ex1 = new Vue({ el: '#ext', });
