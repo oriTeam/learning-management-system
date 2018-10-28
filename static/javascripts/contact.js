164bab944b657e892678cf36039e8838a532f208
lethithanhhoa <32014199+lethithanhhoa@users.noreply.github.com>
2018-10-28 20:12:57 +0700

    Add files via upload

---
+++ static/javascripts/contact.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+    $(".m-input").focus(function(){
+        $(this).css("background-color", "#E6E6E6");
+    });
+    $(".m-input").blur(function(){
+        $(this).css("background-color", "#ffffff");
+        var value = $(this).val();
+        if (value.trim().length == 0) {
+            $(this).css("border", "1px solid red");
+            }
+        else {
+            $(this).css("border", "");
+            $(this).css("opacity","");
+        }
+    });
+});
