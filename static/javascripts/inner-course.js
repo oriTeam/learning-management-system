1978acc5ede357a9bcc2d6065ce5d6b13a0ff8af
vantrong291 <vantrong291@gmail.com>
2018-10-29 11:48:21 +0700

    edit contact page

--- static/javascripts/inner-course.js
+++ static/javascripts/inner-course.js
@@ -1,7 +1,7 @@
 let breadcrumb = '<ul class="bread-crumb px-0">\n' +
     '  <li><a href="/"><span class="fa fa-home"> </span> Trang chủ</a></li>\n' +
-    '  <li><a href="/contact">Khóa học</a></li>\n' +
-    '  <li><a href="/contact">Phát triển ứng dụng web -- INT3306 4</a></li>\n' +
+    '  <li><a href="/course">Khóa học</a></li>\n' +
+    '  <li><a href="/contact">INT3306 4</a></li>\n' +
     '  </ul>';
 
 Vue.component('breadcrumb', {template: breadcrumb });
