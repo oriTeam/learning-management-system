c3fba07a8d0f2a520f2b266419261299cde02131
tuanhah <tuan.a.hoang.1998@gmail.com>
2018-12-15 12:27:43 +0700

    hash password

--- piedu-backend/core/admin.py
+++ piedu-backend/core/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from .models import User
+
+from django.contrib.auth.admin import UserAdmin
+# admin.site.register(CustomUserModel, UserAdmin)
 # from guardian.admin import GuardedModelAdmin
 
 
