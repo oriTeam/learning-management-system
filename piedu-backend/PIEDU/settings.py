96fe80ce47d023502af2dc54238c784ee52520b2
VietNam102 <vantrong291@gmail.com>
2018-12-15 11:27:55 +0700

    update translation

--- piedu-backend/PIEDU/settings.py
+++ piedu-backend/PIEDU/settings.py
@@ -81,8 +81,8 @@ ROOT_URLCONF = 'PIEDU.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
-        # 'APP_DIRS': True,
+        'DIRS': [os.path.join(BASE_DIR, "templates")],
+        'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
                 'django.template.context_processors.debug',
@@ -90,10 +90,10 @@ TEMPLATES = [
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
             ],
-            'loaders': [
-                'django.template.loaders.filesystem.Loader',
-                'django.template.loaders.app_directories.Loader',
-            ]
+            # 'loaders': [
+            #     'django.template.loaders.app_directories.Loader',
+            #     'django.template.loaders.filesystem.Loader',
+            # ]
         },
     },
 ]
